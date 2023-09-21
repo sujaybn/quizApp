88540e5ba1d7c0b280abc2107c123a9273150099
sujaybn <89024189+sujaybn@users.noreply.github.com>
2023-09-22 00:39:55 +0530

    - allowing cross-origin resource sharing

--- src/main/java/com/practice/quizapp/controller/QuizController.java
+++ src/main/java/com/practice/quizapp/controller/QuizController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuizController {
     @Autowired
     QuizService quizService;
